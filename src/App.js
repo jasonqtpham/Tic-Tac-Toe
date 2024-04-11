e9f0ff3731ed40d0bfcd178835095123927593f0
Jason Pham <deh5yb@virginia.edu>
2024-04-10 22:03:35 -0400

    finished time travel feature

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
@@ -79,8 +79,8 @@ export default function Game() {
   }
 
   function jumpTo(nextMove) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
