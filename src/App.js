c0116af4ea326ffc2a5ab0a6368633921c6c11b6
Jason Pham <deh5yb@virginia.edu>
2024-04-13 11:54:15 -0400

    fixed draw bug

--- src/App.js
+++ src/App.js
@@ -125,9 +125,12 @@ function calculateWinner(squares) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
-    } else if (!squares.some(element => element === null)) {
-      return "Draw";
     }
   }
+  
+  if (!squares.some(element => element === null)) {
+    return "Draw";
+  }
+  
   return null;
 }
